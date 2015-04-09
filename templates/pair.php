1e69224a5f414efac61a6bc7d0b81cfe4fc49e81
Ferenc Szekely <ferenc.szekely@gmail.com>
2015-04-09 20:29:48 +0300

    renaming

--- templates/pair.php
+++
@@ -1,8 +0,0 @@
-    <p class="submit">
-        <input type="button" name="submit" id="glome-login" class="button button-primary" value="Pair with Glome Key" />
-    </p>
-
-    <div id="glome-box">
-        <p>Generating identification details for Glome Key.</p>
-        <div id="glome-qr"></div>
-    </div>
---
+++ templates/pairing.php
@@ -0,0 +1,15 @@
+    <div class="code link">
+      <div>Send this link to pair with an other device</div>
+      <div>show url</div>
+    </div>
+
+    <div class="code scan">
+      <div>Scan QR code shown on an other device</div>
+      <div>click scan</div>
+    </div>
+
+    <div class="code enter">
+      <div>Enter code shown on an other device</div>
+      <div>enter code</div>
+    </div>
+
